0f18e29ab8b094f250b51599d87e64df2249ff9b
Michael Shen <michael.sps168@gmail.com>
2020-05-16 10:23:01 +0800

    Initial commit

---
+++ src/main/java/com/michael/Person.java
@@ -0,0 +1,9 @@
+package com.michael;
+
+public class Person {
+    public void hello() {
+
+        System.out.println("Hello World");
+    }
+}
+
---
+++ src/main/kotlin/com/michael/Hello.kt
@@ -0,0 +1,16 @@
+package com.michael
+
+fun main() {
+    //println("Hello Kotlin")
+    //Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human{
+
+    fun hello(){
+
+        println("Hello Kotlin")
+    }
+}
